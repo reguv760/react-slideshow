1aff00b53a8736e108001e226c1137662522a12c
reguv760 <reguv760@gmail.com>
2019-05-20 13:20:01 -0700

    Updates

---
+++ precache-manifest.611f574897326267e85fa056ee9f0b96.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9397a053c9a392ec84995453d154db0b",
+    "url": "/react-slideshow/index.html"
+  },
+  {
+    "revision": "20da3b4e9b6f02db4b4f",
+    "url": "/react-slideshow/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a842297f3ebe759df091",
+    "url": "/react-slideshow/static/css/main.c4ab747f.chunk.css"
+  },
+  {
+    "revision": "20da3b4e9b6f02db4b4f",
+    "url": "/react-slideshow/static/js/2.0faacf28.chunk.js"
+  },
+  {
+    "revision": "a842297f3ebe759df091",
+    "url": "/react-slideshow/static/js/main.d9d27cd1.chunk.js"
+  },
+  {
+    "revision": "6fc3e30cdd0cad892708",
+    "url": "/react-slideshow/static/js/runtime~main.77fd79b3.js"
+  },
+  {
+    "revision": "b04eabf748554997e5f8fcf1e121a838",
+    "url": "/react-slideshow/static/media/bathtub.b04eabf7.svg"
+  },
+  {
+    "revision": "cb48866699d5460b7fd34d6f327976eb",
+    "url": "/react-slideshow/static/media/bed-left-side.cb488666.svg"
+  },
+  {
+    "revision": "6c448f8d142fc631fcf0c7e9ab77fac6",
+    "url": "/react-slideshow/static/media/car-compact.6c448f8d.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-slideshow/static/media/logo.ee7cd8ed.svg"
+  }
+]);
